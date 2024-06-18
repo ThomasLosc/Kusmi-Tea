aa85e3d5514e2929b06f54f0e196e713cd433816
CoralyM <coraly.mgk@gmail.com>
2024-06-18 09:53:44 +0200

    :sparkles: Add SCSS

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,7 @@ require('bootstrap');
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
