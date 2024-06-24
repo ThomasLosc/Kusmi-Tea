721e4cda90c08da954d11132c56363bc12d78ba4
ThomasLosc <thomasthewinner62@gmail.com>
2024-06-24 13:46:02 +0200

    Commande
    Commande

---
+++ src/Entity/Commande.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commandes')]
+    private ?User $user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?float $prix = null;
+
+    #[ORM\Column(type: 'json')]
+    private $productIds = [];
+
+    #[ORM\Column(length: 255)]
+    private ?int $points = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getPoints(): ?string
+    {
+        return $this->points;
+    }
+
+    public function setPoints(int $points): static
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getProductIds(): array
+    {
+        return $this->productIds;
+    }
+
+    public function setProductIds(array $productIds): self
+    {
+        $this->productIds = $productIds;
+
+        return $this;
+    }
+}
